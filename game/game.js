fced613f07270080b4c19bfab31279287d874db8
Logan Absher <absherlogan@gmail.com>
2017-01-23 12:41:46 -0800

    added event listener

--- game/game.js
+++ game/game.js
@@ -1,5 +1,6 @@
 'use strict';
 //arrays of diol
+var phaseArray = ['morning', 'day', 'night'];
 var staticImageArray = ['../images/IMG_0006.jpg'];
 var staticChoiceArray = [['question one', 'question two', 'question three', 'question four']];
 var staticResponseArray = [['response one', 'response two', 'response three', 'response four']];
@@ -15,10 +16,15 @@ function Character(name){
 var player = new Character('name');
 
 function pageConstructor(){
-  staticConstructor(staticImageArray[0], staticChoiceArray[0], staticResponseArray[0]);
+  phaseConstructor(staticImageArray[0], staticChoiceArray[0], staticResponseArray[0]);
 }
 
-function staticConstructor(staticImage, staticChoice, staticResponse){
+function phaseConstructor(staticImage, staticChoice, staticResponse){
+  // for(var j = 0; j < phaseArray.length; j++){
+  //   var titleEl = document.getElementById('head-tag');
+  //   var phaseEl = document.createElement('h1');
+  //   phaseEl.textContent = phaseArray[j];
+  //   titleEl.appendChild(phaseEl);
   var pageEl = document.getElementById('body-tag');
   var imageEl = document.createElement('img');
   imageEl.setAttribute('id', 'background-image');
@@ -30,8 +36,18 @@ function staticConstructor(staticImage, staticChoice, staticResponse){
     var textEl = document.createElement('li');
     textEl.textContent = staticChoice[i];
     listEl.appendChild(textEl);
+    var userChoice = function(event){
+      event.preventDefault();
+      event.stopPropagation();
+      footerEl.removeChild(listEl);
+      var responseEl = document.createElement('p');
+      footerEl.appendChild(staticResponse);
+      console.log(staticResponse);
+    };
+    textEl.addEventListener('click', userChoice, false);
   }
   footerEl.appendChild(listEl);
 }
+// }
 pageConstructor();
 console.log(player.name);
