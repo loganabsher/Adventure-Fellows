cd5fb4a270f6ad28b77b3e5a58a30bec692c0196
Luis Manzo <manzosk3@gmail.com>
2017-01-23 19:43:36 -0800

    solves question iteration issue

--- game/game.js
+++ game/game.js
@@ -39,77 +39,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   renderPage();
 
-
-  //creating a new player with the local storage data
-  // var player = new Character(local);
-  // function random(max, min) {
-  //   return Math.round(Math.random() * (max - min) + min);
-  // }
-  // function bossAdam(image, question, choice, response) {
-  //   interactionImage(bossImageArray);
-  // }
-  // function interactionImage(image) {
-  //   var pageEl = document.getElementById('interactive-box');
-  //   var imageEl = document.createElement('img');
-  //   imageEl.setAttribute('id', 'interactive-image');
-  //   imageEl.setAttribute('src', image);
-  //   pageEl.appendChild(imageEl);
-  // }
-  // function playerImage(image) {
-  //   var pageEl = document.getElementById('user-box');
-  //   var imageEl = document.createElement('img');
-  //   imageEl.setAttribute('id', 'player-image');
-  //   imageEl.setAttribute('src', image);
-  //   pageEl.appendChild(imageEl);
-  // }
-  //short introduction/base page layout
-
-
-  // function intro(image) {
-  //   renderPhaseImage(image);
-  // }
-
-  // function renderPhaseImage(image) {
-  //   var pageEl = document.getElementById('place-image');
-  //   var imageEl = document.createElement('img');
-  //   imageEl.setAttribute('id', 'background-image');
-  //   imageEl.setAttribute('src', image);
-  //   pageEl.appendChild(imageEl);
-  // }
-
-
   function renderPage() {
-    console.log(questionNum)
     renderImage(staticImageArray[questionNum]);
-
-    // renderResponse(, questionNum);
     createDialogue(staticImageArray[questionNum], staticChoiceArray[questionNum], staticResponseArray[questionNum]);
   }
 
-  function renderResponse(id, questionNum) {
-    var questionsList = document.getElementById('questions-list').children;
-    var questionsArray = Array.prototype.slice.call(questionsList);
-    questionsArray.forEach(function (question) {
-      question.remove();
-    })
-    var gameText = document.getElementById('game-text');
-    var responsePar = document.createElement('p');
-    responsePar.setAttribute('id', 'response-paragraph');
-
-    var responseId = parseInt(id);
-    // console.log(responseId);
-
-    responsePar.textContent = staticResponseArray[questionNum][id];
-    gameText.appendChild(responsePar);
-
-    responsePar.addEventListener('click', function () {
-
-
-      console.log('clearing');
-      clearElements();
-      renderPage();
-    });
-  }
+  //RENDERING PAGE
   function renderImage(image) {
     var pageEl = document.getElementById('place-image');
     var imageEl = document.createElement('img');
@@ -121,29 +56,63 @@ document.addEventListener('DOMContentLoaded', function () {
   function createDialogue(image, choices, responses) {
     var gameText = document.getElementById('game-text');
     for (var i = 0; i < choices.length; i++) {
-      var questionEl = document.createElement('li');
-      questionEl.setAttribute('class', 'question');
-      questionEl.setAttribute('id', i);
-      questionEl.textContent = choices[i];
-      var questionsList = document.getElementById('questions-list');
-      questionsList.appendChild(questionEl);
+      var choiceEl = document.createElement('li');
+      choiceEl.setAttribute('class', 'question');
+      choiceEl.setAttribute('id', i);
+      choiceEl.textContent = choices[i];
+      var choicesList = document.getElementById('choices-list');
+      choicesList.appendChild(choiceEl);
     }
+    handleChoiceClick();      //
 
-    var questionsCollection = document.getElementById('questions-list').children;
-    var questionsArray = Array.prototype.slice.call(questionsCollection);
+  }
 
-    questionsArray.forEach(function (question) {
-      question.addEventListener('click', function () {
-        console.log('click');
+  function handleChoiceClick() {    //renders the response
+    var choicesCollection = document.getElementById('choices-list').children;
+    var choicesArray = Array.prototype.slice.call(choicesCollection);
+
+    choicesArray.forEach(function (choice) {
+      choice.addEventListener('click', function () {
         renderResponse(this.id, questionNum);
-        console.log('incrementing');
       });
     });
 
-    // var questions = Array.prototype.slice.call(questionsCollection); //turn into array
-    // console.log(questionsCollection);
   }
+
+  function removeChoiceElements() {
+    var choicesList = document.getElementById('choices-list').children;
+    var choicesArray = Array.prototype.slice.call(choicesList);
+    choicesArray.forEach(function (choice) {
+      choice.remove();
+    });
+  }
+
+  function renderResponse(id, questionNum) {
+    removeChoiceElements();
+
+    var gameText = document.getElementById('game-text');  //append response to textContent div
+    var responsePar = document.createElement('p');
+    responsePar.setAttribute('id', 'response-paragraph');
+
+    var responseId = parseInt(id);
+    // console.log(responseId);
+    responsePar.textContent = staticResponseArray[questionNum][id];
+    gameText.appendChild(responsePar);
+
+    if (questionNum < staticQuestionArray.length - 1) {
+      responsePar.addEventListener('click', function () {   //when click, clear DOM elements and render new
+        clearElements();
+        renderPage();
+      });
+    } else {
+      renderTransition();
+    }
+  }
+
   function clearElements() {
+    questionNum++;              //after rendering response, move to next question
+
+    console.log('incrementing...now on question at index: ', questionNum);
 
     var response = document.getElementById('response-paragraph');
     console.log(response);
@@ -151,8 +120,10 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('removing image');
     image.remove();
     response.remove();
-    questionNum++;
+  }
 
+  function renderTransition() {
+    
   }
 
 });
