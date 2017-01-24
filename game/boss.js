d797dc806bb1a420e761b2cf1e3d63d9f606049e
Logan Absher <absherlogan@gmail.com>
2017-01-24 15:03:44 -0800

    reconfigured boss.js

--- game/boss.js
+++ game/boss.js
@@ -2,24 +2,17 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
   var questionNum = 0;
-  //array of place images
-  var staticImageArray = ['../images/cf_building.jpg', '../images/cf_building.jpg', '../images/cf_building.jpg'];
-  //array of questions/events
-  var staticQuestionArray = ['You are on your home directory of the terminal and following along, what do you type in the command line?', 'How do you spend the weekend?'];
-  //array of choices for the questions
-  var staticChoiceArray = [['rm –rf', 'cd codefellows/201', 'tree', 'cmatrix'], ['Go to the spa', 'study all weekend', 'go out for dinner and drinks with friends', 'sleep your standard eight hours, run, study']];
-  //array of responses to the choices
-  var staticResponseArray = [['You deleted all files on your machine, you can no longer continue in the class. (-100, health, -100 grade)', 'You follow along with the class, learing much about how to properly operate your computer.', 'You tree from your home directory, the files keep flying past your screen, it amazes you how many "interesting" files are on your computer ;). (+5 grade, -10 social)', 'You cmatrix and stare at the screen mesmorized by the falling matrix, you pay little attention to the lecture. (-10 grade)'], ['You go to the spa to rejuvenate and relax, sleeping in and lounging all weekend. (+20 health, -10 grade, +10 social)', 'You study very hard all weekend, not getting a chance to relax or see any friends. (+10 grade, - 15 social)', 'You go out drinking all weekend having a terrible hangover, but somehow on Monday your code is finished? (-15 health, +5 grade, +20 social)', 'You get your standard eight hours of sleep, go for a run, study and finish your homework like a productive member of society. (+5 health, +5 grade, -5 social)']];
-
-  var uniqueClassPerResponse = [['rm-rf', 'cdCorrectly', 'tree', 'cmatrix'], ['spa', 'studyWeekend', 'dinner', 'sleepEight']];
-  // increments score
-  var affectScore = [[[-100, -100, -100], [0, 0, +25], [0, 0, 0], [0, -25, 0]], [[+25, -25, 0], [-25, +25, -25], [-25, -25, +25], [+10, +10, +10]]];
-  //array of randomly chosen questions
-  var randomQuestionArray = [];
-  //corresponding choices to the questions
-  var randomChoiceArray = [];
-  //corresponding responses to the choices
-  var randomResponseArray = [];
+  //array of boss images
+  var bossImageArray = ['../images/angrierAdam.jpg', '../images/angryAdam.jpg', '../images/normalAdam.jpg', '../images/happyAdam.jpg','../images/happierAdam.jpg'];
+  //array of boss questions
+  var bossQuestionArray = ['Adam uses busmall, what is your response?', 'Adam uses salmon cookies, what is your response?', 'Adam uses chocolate pizza, what is your response?', 'Adam uses about me, what is your response?', 'Adam uses disapointment, what is your response?'];
+  //corresponding choices to questions
+  var bossChoiceArray = [['bussmall', 'bussmall', 'bussmall', 'bussmall'], ['salmon cookies', 'salmon cookies', 'salmon cookies', 'salmon cookies'], ['chocolate pizza', 'chocolate pizza', 'chocolate pizza', 'chocolate pizza'], ['about me', 'about me', 'about me', 'about me'], ['disapointment', 'disapointment', 'disapointment', 'disapointment']];
+  //corresponding responses to choices
+  var bossResponseArray = [['bussmall', 'bussmall', 'bussmall', 'bussmall'], ['salmon cookies', 'salmon cookies', 'salmon cookies', 'salmon cookies'], ['chocolate pizza', 'chocolate pizza', 'chocolate pizza', 'chocolate pizza'], ['about me', 'about me', 'about me', 'about me'], ['disapointment', 'disapointment', 'disapointment', 'disapointment']];
+  //corresponding effects per response
+  var affectScore = [[[-100, -100, -100], [0, 0, +25], [0, 0, 0], [0, -25, 0]], [[+25, -25, 0]]];
+  var uniqueClassPerResponse = [['bussmall', 'bussmall', 'bussmall', 'bussmall'], ['salmon cookies', 'salmon cookies', 'salmon cookies', 'salmon cookies'], ['chocolate pizza', 'chocolate pizza', 'chocolate pizza', 'chocolate pizza'], ['about me', 'about me', 'about me', 'about me'], ['disapointment', 'disapointment', 'disapointment', 'disapointment']];
   //collecting local storage from character page
   // var local = JASN.parse(localStorage);
   // console.log(local);
@@ -38,9 +31,9 @@ document.addEventListener('DOMContentLoaded', function () {
   renderPage();
 
   function renderPage() {
-    renderImage(staticImageArray[questionNum]);
+    renderImage(bossImageArray[questionNum]);
     displayQuestionPrompt(questionNum);
-    createDialogue(staticImageArray[questionNum], staticChoiceArray[questionNum], staticResponseArray[questionNum]);
+    createDialogue(bossImageArray[questionNum], bossChoiceArray[questionNum], bossResponseArray[questionNum]);
   }
 
   //RENDERING PAGE
@@ -56,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var questionContainer = document.getElementById('prompt');
     var displayQuestionEl = document.createElement('p');
     displayQuestionEl.setAttribute('id', 'questionNum');
-    displayQuestionEl.textContent = staticQuestionArray[questionNum];
+    displayQuestionEl.textContent = bossQuestionArray[questionNum];
     questionContainer.appendChild(displayQuestionEl);
   }
 
@@ -125,10 +118,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var responseId = parseInt(id);
     // console.log(responseId);
-    responsePar.textContent = staticResponseArray[questionNum][id];
+    responsePar.textContent = bossResponseArray[questionNum][id];
     gameText.appendChild(responsePar);
 
-    if (questionNum < staticQuestionArray.length - 1) {
+    if (questionNum < bossQuestionArray.length - 1) {
       responsePar.addEventListener('click', function () {   //when click, clear DOM elements and render new
         clearElements();
         renderPage();
@@ -161,13 +154,3 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
-// //array of boss images
-// var bossImageArray = ['../images/angrierAdam.jpg', '../images/angryAdam.jpg', '../images/normalAdam.jpg', '../images/happyAdam.jpg','../images/happierAdam.jpg'];
-// //array of boss questions
-// var bossQuestionArray = ['Adam uses busmall, what is your response?', 'Adam uses salmon cookies, what is your response?', 'Adam uses chocolate pizza, what is your response?', 'Adam uses about me, what is your response?'];
-// //corresponding choices to questions
-// var bossChoiceArray = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']];
-// //corresponding responses to choices
-// var bossResponseArray = [['hello', 'hello', 'hello', 'hello'], ['hello', 'hello', 'hello', 'hello'], ['hello', 'hello', 'hello', 'hello'], ['hello', 'hello', 'hello', 'hello']];
-// //corresponding effects per response
-// var bossEffectArray = [];
