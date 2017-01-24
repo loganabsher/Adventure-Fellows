980ec073acd51a561a1db5e4612eeb0279efd1d1
David Teddy <davidteddy@outlook.com>
2017-01-24 11:20:01 -0800

    new stuff

--- game/game.js
+++ game/game.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
   //array of place images
   var staticImageArray = ['../images/cf_building.jpg', '../images/cf_building.jpg', '../images/cf_building.jpg'];
   //array of questions/events
-  var staticQuestionArray = ['you are on your home directory of the terminal and following along, what do you type in the command line?', 'how do you spend the weekend?'];
+  var staticQuestionArray = ['This is your first day of Code Fellow\'s. You are terrified and apprehensive because you know no one in your class. For the first hour of you being at the most amazing school ever, there is an orientation. Brooks is so excited to meet everyone he looks like he just merges with the code. After orientation you and your fellow class mates got to class and meet eachother. It may be scary at first to intorduce yourself but get comfertable when you realize that everone is just as scared as you. Adams beard blows your mind and Sams bald head blinds you. You go home knowing a little more than you realize and had fun making new friends and gaining a lot of knowledge.','you are on your home directory of the terminal and following along, what do you type in the command line?', 'how do you spend the weekend?'];
   //array of choices for the questions
-  var staticChoiceArray = [['rm –rf', 'cd codefellows/201', 'tree', 'cmatrix'], ['Go to the spa', 'study all weekend', 'go out for dinner and drinks with friends', 'sleep your standard eight hours, run, study']];
+  var staticChoiceArray = [['next'], ['rm –rf', 'cd codefellows/201', 'tree', 'cmatrix'], ['Go to the spa', 'study all weekend', 'go out for dinner and drinks with friends', 'sleep your standard eight hours, run, study']];
   //array of responses to the choices
-  var staticResponseArray = [['you deleted all files on your machine, you can no longer continue in the class. (-100, health, -100 grade)', 'you follow along with the class, learing much about how to properly operate your computer.', 'you tree from your home directory, the files keep flying past your screen, it amazes you how many "interesting" files are on your computer ;). (+5 grade, -10 social)', 'you cmatrix and stare at the screen mesmorized by the falling matrix, you pay little attention to the lecture. (-10 grade)'], ['you go to the spa to rejuvenate and relax, sleeping in and lounging all weekend. (+20 health, -10 grade, +10 social)', 'you study very hard all weekend, not getting a chance to relax or see any friends. (+10 grade, - 15 social)', 'you go out drinking all weekend having a terrible hangover, but somehow on Monday your code is finished? (-15 health, +5 grade, +20 social)', 'you get your standard eight hours of sleep, go for a run, study and finish your homework like a productive member of society. (+5 health, +5 grade, -5 social)']];
+  var staticResponseArray = [['The next day'], ['you deleted all files on your machine, you can no longer continue in the class. (-100, health, -100 grade)', 'you follow along with the class, learing much about how to properly operate your computer.', 'you tree from your home directory, the files keep flying past your screen, it amazes you how many "interesting" files are on your computer ;). (+5 grade, -10 social)', 'you cmatrix and stare at the screen mesmorized by the falling matrix, you pay little attention to the lecture. (-10 grade)'], ['you go to the spa to rejuvenate and relax, sleeping in and lounging all weekend. (+20 health, -10 grade, +10 social)', 'you study very hard all weekend, not getting a chance to relax or see any friends. (+10 grade, - 15 social)', 'you go out drinking all weekend having a terrible hangover, but somehow on Monday your code is finished? (-15 health, +5 grade, +20 social)', 'you get your standard eight hours of sleep, go for a run, study and finish your homework like a productive member of society. (+5 health, +5 grade, -5 social)']];
   //array of randomly chosen questions
   var randomQuestionArray = [];
   //corresponding choices to the questions
@@ -26,17 +26,15 @@ document.addEventListener('DOMContentLoaded', function () {
     this.grade = 100;
     this.social = 100;
   }
-
   var character = new Character(localStorage.userName, localStorage.imgUrl);
   console.log(character);
-
   renderPage();
-  
+
   function renderPage() {
     renderImage(staticImageArray[questionNum]);
+    displayQuestionPrompt(questionNum);
     createDialogue(staticImageArray[questionNum], staticChoiceArray[questionNum], staticResponseArray[questionNum]);
   }
-
   //RENDERING PAGE
   function renderImage(image) {
     var pageEl = document.getElementById('place-image');
@@ -45,7 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
     imageEl.setAttribute('src', image);
     pageEl.appendChild(imageEl);
   }
-
+  function displayQuestionPrompt(questionNum) {
+    var questionContainer = document.getElementById('prompt');
+    var displayQuestionEl = document.createElement('p');
+    displayQuestionEl.setAttribute('id', 'questionNum');
+    displayQuestionEl.textContent = staticQuestionArray[questionNum];
+    questionContainer.appendChild(displayQuestionEl);
+  }
   function createDialogue(image, choices, responses) {
     var gameText = document.getElementById('game-text');
     for (var i = 0; i < choices.length; i++) {
@@ -57,21 +61,21 @@ document.addEventListener('DOMContentLoaded', function () {
       choicesList.appendChild(choiceEl);
     }
     handleChoiceClick();      //
-
   }
-
   function handleChoiceClick() {    //renders the response
     var choicesCollection = document.getElementById('choices-list').children;
     var choicesArray = Array.prototype.slice.call(choicesCollection);
-
     choicesArray.forEach(function (choice) {
       choice.addEventListener('click', function () {
         renderResponse(this.id, questionNum);
       });
     });
-
   }
-
+  function removePrompt() {
+    var promptParent = document.getElementById('prompt');
+    var promptChild = document.getElementById('questionNum');
+    promptParent.removeChild(promptChild);
+  }
   function removeChoiceElements() {
     var choicesList = document.getElementById('choices-list').children;
     var choicesArray = Array.prototype.slice.call(choicesList);
@@ -79,19 +83,16 @@ document.addEventListener('DOMContentLoaded', function () {
       choice.remove();
     });
   }
-
   function renderResponse(id, questionNum) {
     removeChoiceElements();
-
+    removePrompt();
     var gameText = document.getElementById('game-text');  //append response to textContent div
     var responsePar = document.createElement('p');
     responsePar.setAttribute('id', 'response-paragraph');
-
     var responseId = parseInt(id);
     // console.log(responseId);
     responsePar.textContent = staticResponseArray[questionNum][id];
     gameText.appendChild(responsePar);
-
     if (questionNum < staticQuestionArray.length - 1) {
       responsePar.addEventListener('click', function () {   //when click, clear DOM elements and render new
         clearElements();
@@ -101,12 +102,9 @@ document.addEventListener('DOMContentLoaded', function () {
       renderTransition();
     }
   }
-
   function clearElements() {
     questionNum++;              //after rendering response, move to next question
-
     console.log('incrementing...now on question at index: ', questionNum);
-
     var response = document.getElementById('response-paragraph');
     console.log(response);
     var image = document.getElementById('background-image');
@@ -114,13 +112,11 @@ document.addEventListener('DOMContentLoaded', function () {
     image.remove();
     response.remove();
   }
-
-  function renderTransition() {           //reveals a hidden link to transition to week2   
+  function renderTransition() {           //reveals a hidden link to transition to week2
     var jCharacter = JSON.stringify(character); //wraps up character in JSON to send through
     localStorage.character = jCharacter;
-    console.log(jCharacter);              
+    console.log(jCharacter);
     var hiddenLink = document.getElementById('link-to-week2');
     hiddenLink.removeAttribute('class', 'hidden');
   }
-
 });
