f9bf99b416d4ca1d04a3f8b5bf357f79e3d94b0b
Allie Grampa <agrampa@yahoo.com>
2017-01-24 17:43:18 -0800

    stretch almost working but kinda not

--- game/game.js
+++ game/game.js
@@ -11,14 +11,22 @@ document.addEventListener('DOMContentLoaded', function () {
   var staticResponseArray = [['You deleted all files on your machine, you can no longer continue in the class. (-100, health, -100 grade)', 'You follow along with the class, learing much about how to properly operate your computer.', 'You tree from your home directory, the files keep flying past your screen, it amazes you how many "interesting" files are on your computer ;). (+5 grade, -10 social)', 'You cmatrix and stare at the screen mesmorized by the falling matrix, you pay little attention to the lecture. (-10 grade)'], ['You go to the spa to rejuvenate and relax, sleeping in and lounging all weekend. (+20 health, -10 grade, +10 social)', 'You study very hard all weekend, not getting a chance to relax or see any friends. (+10 grade, - 15 social)', 'You go out drinking all weekend having a terrible hangover, but somehow on Monday your code is finished? (-15 health, +5 grade, +20 social)', 'You get your standard eight hours of sleep, go for a run, study and finish your homework like a productive member of society. (+5 health, +5 grade, -5 social)']];
 
   var uniqueClassPerResponse = [['rm-rf', 'cdCorrectly', 'tree', 'cmatrix'], ['spa', 'studyWeekend', 'dinner', 'sleepEight']];
-  // increments score
+  // increments score  pairpgrammed with Teddy
   var affectScore = [[[-100, -100, -100], [0, 0, +25], [0, 0, 0], [0, -25, 0]], [[+25, -25, 0], [-25, +25, -25], [-25, -25, +25], [+10, +10, +10]]];
-  //array of randomly chosen questions
-  var randomQuestionArray = [];
-  //corresponding choices to the questions
-  var randomChoiceArray = [];
-  //corresponding responses to the choices
-  var randomResponseArray = [];
+
+  var increaseHealth = ['You go to the pharmacy and get a flu shot! Health increases', 'You get a good night’s sleep! Health increases', 'You decide to take a break! Health increases', 'You finish your project and leave early! Health increases', 'You have time to go to the gym! Health increases'];
+
+  var decreaseHealth = ['You don\'t wash your hands and end up with the flu! Health decreases', 'Who has time to cook? Eat fast food instead! Health decreases', 'Stressed? Here\'s a case of insomnia! Health decreases', 'Work nonstop for 8 hours, who needs sleep anyway!? Health decreases', 'Walking home from school, pull a muscle! Health decreases'];
+
+  var increaseGrade = ['Whoa! You aced your project! Grade increases', 'Your functions work on the first try! Grade increases', 'Your table renders properly on the first try! Grade increases', 'Chart.js renders a perfect chart. Look at those bars! Grade increases', 'Caffeination is perfect all day! Grade increases'];
+
+  var decreaseGrade = ['You forget to ACP and lose all your code! Grade decreases', 'Oh no! You oversleep and miss code review and lecture! Grade decreases', 'Whoops! You forget to appendChild and your table looks like garbage! Grade decreases', 'You just broke your code and can’t figure out how to fix it! Grade decreases', 'Console.log returns NaN! What!? Grade decreases'];
+
+  var increaseSocial = ['You sit with your classmates at lunch, make a new friend! Social increases', 'You help a classmate who is stuck, you\'re so nice! Social increases', 'A classmate is new to Seattle, you refer them to a great restaurant! Social increases', 'Go grab coffee with a classmate! Social increases', 'Share a snack with a classmate who is hangry, what a good friend! Social increases'];
+
+  var decreaseSocial = ['You sit alone at lunch, man that sucks! Social decreases', 'A classmates asks for help, you say no. What a jerk! Social decreases', 'You\'re too tired to shower, develop B.O., no one wants to sit near you! Social decreases', 'You laugh when a classmate’s code doesn’t work. You\'re a terrible person! Social decreases', 'A classmate is new to Seattle, refer them to a bad restaurant. That\'s so mean! Social decreases'];
+
+  var randomArrays = [increaseHealth, decreaseHealth, increaseGrade, decreaseGrade, increaseSocial, decreaseSocial];
   //collecting local storage from character page
   // var local = JASN.parse(localStorage);
   // console.log(local);
@@ -76,8 +84,11 @@ document.addEventListener('DOMContentLoaded', function () {
   function updateStats(responseIndex) {
     var responseIndex = parseInt(responseIndex);
     var character = JSON.parse(localStorage.character);
-    console.log(character);
+    console.log('character ' + character);
     for (var i = 0; i < affectScore.length; i++) {
+      console.log('affectScore: ' + affectScore);
+      console.log('affectScore[responseIndex]' + affectScore[i][responseIndex]);
+      console.log(responseIndex);
       character.health = character.health + affectScore[responseIndex][i][0];
       console.log('this.health ' + character.health);
       character.grade = character.grade + affectScore[responseIndex][i][1];
@@ -85,6 +96,7 @@ document.addEventListener('DOMContentLoaded', function () {
       character.social = character.social + affectScore[responseIndex][i][2];
       console.log('this.social ' + character.social);
     }
+    updateWithRandom(responseIndex);
   }
   //pair programmed with EVERYONE
 
@@ -148,6 +160,7 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('removing image');
     image.remove();
     response.remove();
+    trulyRandom();
   }
 
   function renderTransition() {           //reveals a hidden link to transition to week2
@@ -159,4 +172,48 @@ document.addEventListener('DOMContentLoaded', function () {
     // hiddenLink.removeAttribute('class', 'hidden');
   }
 
+  var randomNumberArray = Math.floor(Math.random() * 6);
+  var randomNumberPrompt = Math.floor(Math.random() * 5);
+
+  function displayRandomEvent() {
+    alert (randomArrays[randomNumberArray][randomNumberPrompt]);
+    console.log(randomArrays[randomNumberArray][randomNumberPrompt]);
+  }
+//pairprogrammed with Teddy
+  var generateRandom = Math.random();
+
+  function trulyRandom() {
+    if (generateRandom > 0.5) {
+      displayRandomEvent();
+    }
+  }
+
+  function updateWithRandom(responseIndex) {
+
+    console.log('we made it!');
+    var responseIndex = parseInt(responseIndex);
+    var character = JSON.parse(localStorage.character);
+    if (increaseHealth.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.health = character.health + 10;
+      console.log(character.health);
+    } else if (decreaseHealth.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.health = character.health - 10;
+      console.log(character.health);
+    } else if (increaseGrade.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.grade = character.grade + 10;
+      console.log(character.grade);
+    } else if (decreaseGrade.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.grade = character.grade - 10;
+      console.log(character.grade);
+    } else if (increaseSocial.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.social = character.social + 10;
+      console.log(character.social);
+    } else if (decreaseSocial.includes(randomArrays[randomNumberArray][randomNumberPrompt])) {
+      character.social = character.social - 10;
+      console.log(character.social);
+    } else {
+      console.log('oops!');
+    }
+  }
 });
+
